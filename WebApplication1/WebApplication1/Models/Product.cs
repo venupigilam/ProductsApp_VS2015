e1f92411c402e84c98f8c4ce1294e4f156918771
venupigilam <venupigilam@gmail.com>
2018-02-10 14:57:21 +0530

    pr

--- WebApplication1/WebApplication1/Models/Product.cs
+++ WebApplication1/WebApplication1/Models/Product.cs
@@ -8,7 +8,7 @@ namespace WebApplication1.Models
     public class Product
     {
         public int Id { get; set; }
-        public string name { get; set; }
+        public string name { get; set; } 
         public string Category { get; set; }
         public decimal Price { get; set; }
 
