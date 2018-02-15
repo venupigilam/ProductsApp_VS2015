975b4c16c6425e6f5433c30a84cc931bbff1ea02
venupigilam <venupigilam@gmail.com>
2018-02-15 23:37:55 +0530

    ListQueue and ListStack Practice

---
+++ WebApplication1/WebApp/Pages/ListQueueandListStack/WebForm3.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebFormsDemo;
+
+namespace WebApp.Pages.ListQueueandListStack
+{
+    public partial class WebForm3 : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("WebForm3");
+        }
+    }
+}
